58bbd48d8e70a0784062d8581e0426e7ced4354c
jakariamasum <jmphy698@gmail.com>
2023-10-30 18:05:06 +0600

    team's social icon add

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Title from '../Title/Title';
 import TeamCarousal from './TeamCarousal';
 
--- src/components/Team/TeamCard.jsx
+++ src/components/Team/TeamCard.jsx
@@ -1,14 +1,37 @@
+import {FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { useState } from 'react';
 
 const TeamCard = ({ img, name, position }) => {
-    return (
-        <div className='flex flex-col gap-4'>
-            <img src={img} alt="" className='rounded' />
-            <div>
-                <h1 className='text-2xl'>{name}</h1>
-                <p className='text-[#ff7425] mb-7'>{position}</p>
-            </div>
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      className='flex flex-col gap-4 relative'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img src={img} alt="" className='rounded' />
+      {isHovered && (
+        <div className='absolute bottom-1/3 left-1/3  flex gap-2 items-center'>
+          <FaFacebookF fill='white' size={32} className='bg-[#ff7425] rounded-[50%] py-2 cursor-pointer hover:bg-[#242628]'/>
+          <FaTwitter fill='white' size={32} className='bg-[#ff7425] rounded-[50%] py-2 cursor-pointer hover:bg-[#242628]'/>
+          <FaInstagram fill='white' size={32} className='bg-[#ff7425] rounded-[50%] py-2 cursor-pointer hover:bg-[#242628]'/>
         </div>
-    );
+      )}
+      <div>
+        <h1 className='text-2xl'>{name}</h1>
+        <p className='text-[#ff7425] mb-7'>{position}</p>
+      </div>
+    </div>
+  );
 };
 
 export default TeamCard;
