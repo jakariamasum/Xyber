780378fa5960ff23a41e64144226e9887e0db94d
jakariamasum <jmphy698@gmail.com>
2023-10-30 08:38:40 +0600

    watch section add

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Footer from "./components/Footer/Footer"
 import Plans from "./components/Plans/Plans"
 import Questions from "./components/Questions/Questions"
 import Testimonials from "./components/Testimonials/Testimonials"
+import Watch from "./components/Watch/Watch"
 
 function App() {
 
   return (
     <div>
+      <Watch/>
       <Questions/>
       <Plans/>
       <Testimonials/>
