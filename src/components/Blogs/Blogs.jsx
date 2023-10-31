59c9607c1ff9c05674cd115b4a49b37c93b46f54
jakariamasum <jmphy698@gmail.com>
2023-10-31 13:47:49 +0600

    about section responsive

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,9 +5,9 @@ import blog2 from '../../assets/blog2.jpg'
 import blog3 from '../../assets/blog3.jpg'
 const Blogs = () => {
     return (
-        <div className="my-20 px-32">
+        <div className="my-20 lg:px-32">
             <Title subtitle={'Our Blog'} title={'Latest Blog & News'}/>
-            <div className="flex gap-4 mt-11">
+            <div className="flex flex-col mx-1 md:flex-row gap-4 mt-11">
                 <BlogCard img={blog1} title={'Cras Accumsan Nulla Nec Lacus Ultricies Placerat.'} desc={'Curabitur sagittis libero tincidunt tempor finibus. Mauris at dignissim ligula, nec tristique orci.'} date={' 24th March 2023'}/>
                 <BlogCard img={blog2} title={'Dras Accumsan Nulla Nec Lacus Ultricies Placerat.'} desc={'Curabitur sagittis libero tincidunt tempor finibus. Mauris at dignissim ligula, nec tristique orci.'} date={' 25th March 2023'}/>
                 <BlogCard img={blog3} title={'Seas Accumsan Nulla Nec Lacus Ultricies Placerat.'} desc={'Curabitur sagittis libero tincidunt tempor finibus. Mauris at dignissim ligula, nec tristique orci.'} date={' 25th March 2023'}/>
